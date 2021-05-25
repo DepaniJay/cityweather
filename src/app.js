08a1a6c7a1aece8540c6575dffe3a0d05625554e
DepaniJay <jrdepani@gmail.com>
2021-05-25 23:03:11 +0530

    solve some grammer misteks

--- src/app.js
+++ src/app.js
@@ -34,7 +34,7 @@ app.get('/weather',(req,res)=>{
 
 app.get('*',(req,res)=>{
     res.render('404',{
-        errorMsg :"Sorry, Page Not Found",
+        errorMsg :"Sorry, Page Not Found.",
         currentYear
     });
 });
